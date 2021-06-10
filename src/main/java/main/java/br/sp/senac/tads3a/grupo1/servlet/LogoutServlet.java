91879401a56612935120555ad869eb22a0d5f41f
wmdbox <wesley132013@gmail.com>
2021-06-09 22:48:16 -0300

    Alteração

--- src/main/java/main/java/br/sp/senac/tads3a/grupo1/servlet/LogoutServlet.java
+++ src/main/java/main/java/br/sp/senac/tads3a/grupo1/servlet/LogoutServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * @author Wesley
+ */
 public class LogoutServlet extends HttpServlet {
 
     @Override
